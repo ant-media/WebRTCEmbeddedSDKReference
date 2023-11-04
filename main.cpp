a4f9197688be9f3f68c3c9715f9417266e899939
root <root@ant>
2023-11-05 00:26:41 +0300

    update arm library

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main(int argc, char* argv[]) {
             setTurnServerParams(true, argv[5], argv[6], argv[7]);
         }
 
+	setMode(1);
         start(argv[1], argv[2], argv[3]);
     }
     else {
